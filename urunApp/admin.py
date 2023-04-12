6bea9e4072b0b53c5e05376de1a5876d4a775563
_omerock <83617943+AysKrimn@users.noreply.github.com>
2023-04-12 11:20:29 +0300

    Delete urunApp directory

--- urunApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Modellerini admin sayfasına kayıt et
-admin.site.register(Urun)
-admin.site.register(Yorum)
--- urunApp/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class CreateUrun(forms.ModelForm):
-      class Meta:
-            model=Urun
-            fields=["ad", "urunDetayi", "fiat", "seriNo", "urunResmi"]
